a50e50d0c8cde668b1f6eb253fe859b4919f495e
Kengston <danidc@mail.ru>
2024-02-07 16:28:39 +0700

    Add Doctrine Fixtures bundle and data fixtures
    Added the Doctrine Fixtures bundle as a dev dependency and created new data fixtures for App and Movie entities. This allows developers to load predefined sets of data useful for testing and development purposes. Also, some unnecessary migration files were deleted and a new database schema migration was generated.

--- src/Entity/Actor.php
+++ src/Entity/Actor.php
@@ -8,7 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ActorRepository::class)]
-class Actor
+class   Actor
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
