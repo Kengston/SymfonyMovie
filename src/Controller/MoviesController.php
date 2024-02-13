33ac2bd5e2f1ade5c8cbb0fcd4a6a4e66b236a84
Kengston <danidc@mail.ru>
2024-02-13 17:55:41 +0700

    Add movie review related features
    Added multiple new templates for movie reviews including create, show, and edit. Refactored MoviesController to use the MovieRepository for accessing the database. Also added support for image file loading in the webpack configuration. These additions and changes enhance the functionality related to movies review in the application by allowing

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Movie;
+use App\Repository\MovieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,12 +11,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    private $em;
-    public function __construct(EntityManagerInterface $em) {
-        $this->em = $em;
+    private $movieRepository;
+
+    public function __construct(MovieRepository $movieRepository)
+    {
+        $this->movieRepository = $movieRepository;
     }
 
-    #[Route('/movies', name: 'app_movies')]
+    #[Route('/movies', methods: ['GET'],  name: 'app_movies')]
     public function index(): Response
     {
         // findAll - SELECT * FROM REPOSITORY;
@@ -25,10 +28,29 @@ class MoviesController extends AbstractController
         // title = "The Dark Knight" ORDER BY id DESC
         // count() - SELECT COUNT() from movies WHERE id = 6
         // getClassName()
-        $repository = $this->em->getRepository(Movie::class);
 
-        $movies = $repository->findAll();
+        $movies = $this->movieRepository->findAll();
 
-        return $this->render('movies/index.html.twig');
+        return $this->render('movies/index.html.twig', [
+            'movies' => $movies
+        ]);
+    }
+
+    #[Route('/movies/{id}', methods: ['GET'], name: 'app_movie')]
+    public function show($id): Response
+    {
+        // findAll - SELECT * FROM REPOSITORY;
+        // find() - SELECT * from movies WHERE id = 5;
+        // findBy() - SELECT * from movies ORDER BY DESC
+        // findOneBy() - SELECT * form movies WHERE id = 6 AND
+        // title = "The Dark Knight" ORDER BY id DESC
+        // count() - SELECT COUNT() from movies WHERE id = 6
+        // getClassName()
+
+        $movie = $this->movieRepository->find($id);
+
+        return $this->render('movies/show.html.twig', [
+            'movie' => $movie
+        ]);
     }
 }
